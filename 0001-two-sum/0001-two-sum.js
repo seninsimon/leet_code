f5b399bbec2a550a5b288d5937298af6558d411a
senin c simon <118550960+seninsimon@users.noreply.github.com>
2025-12-07 22:42:03 +0530

    Time: 34 ms (26.34%), Space: 54 MB (97.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+
+    let index1
+    let index2
+    for(let i = 0 ; i < nums.length ; i++)
+    {
+        for(let j = i + 1 ; j < nums.length ; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                index1 = i
+                index2 = j
+                return [index1 , index2]
+            }
+        }
+    }
+
+
+    
+};
