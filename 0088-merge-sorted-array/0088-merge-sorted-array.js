23378ea282638b6cd52bacd08c3c1c7103354799
senin c simon <118550960+seninsimon@users.noreply.github.com>
2026-04-09 22:47:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,15 +5,35 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
 
-      array1 = nums1.slice(0,m)
-      array2 = nums2.slice(0,n)
-      let merged = [...array1 , ...array2].sort((a,b)=>(a-b))
-      for(let i = 0 ; i < merged.length ; i++)
-      {
-        nums1[i] = merged[i]
-      }
+    //   array1 = nums1.slice(0,m)
+    //   array2 = nums2.slice(0,n)
+    //   let merged = [...array1 , ...array2].sort((a,b)=>(a-b))
+    //   for(let i = 0 ; i < merged.length ; i++)
+    //   {
+    //     nums1[i] = merged[i]
+    //   }
 
-      
+    let i = m - 1
+    let j = n - 1
+    let k = m + n - 1
+
+    console.log(i, j, k)
+
+    while (j >= 0) {
+        if ( nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i]
+            i--
+        }
+        else {
+            nums1[k] = nums2[j]
+            j--
+        }
+        k--
+    }
+
+
+
+
 };
