9b39171004d40d4a101f336f1e13b67ed01f615b
senin c simon <118550960+seninsimon@users.noreply.github.com>
2026-04-28 16:46:04 +0530

    Time: 1 ms (77.12%), Space: 56 MB (21.58%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,31 +4,19 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let unique1 = new Set(nums1)
-    console.log([...unique1])
-    let unique2 = new Set(nums2)
-    console.log([...unique2])
-    let returnarray = []
-    for(let i = 0 ; i < nums1.length ; i++ )
+
+    let arr =[...new Set(nums1)]
+    let arr2 = new Set(nums2)
+    let result = []
+
+    for(let x of arr)
     {
-        for(let j = 0 ; j < nums2.length ; j++)
-        {
-            if(nums1[i] == nums2[j])
-            {
-                returnarray.push(nums1[i])
-            }
-        }
+       if(arr2.has(x))
+       {
+        result.push(x)
+       }
     }
 
-    console.log(returnarray)
-
-    const array = new Set(returnarray)
-
-
-    const unique = [...array]
-    console.log([...array])
-
-    return unique
-
+    return result
     
 };
