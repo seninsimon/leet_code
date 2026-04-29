b62dcdd7b3a85f276feeb1820370be6c4e9b2f51
senin c simon <118550960+seninsimon@users.noreply.github.com>
2026-04-29 15:58:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,28 +4,45 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
+
     let map = {}
-    
+
     for (let x of nums) {
         map[x] = (map[x] || 0) + 1
     }
 
-    let frequencies = Object.values(map)
-    console.log(frequencies)
-    let sorted = [...new Set(frequencies)].sort((a, b) => b - a)
-    console.log(sorted)
+
+
+    let array = []
+
+    for (let key in map) {
+        array.push(map[key])
+    }
+
+    let sorted = array.sort((a, b) => b - a)
+
 
     let result = []
 
-    for (let freq of sorted) {
-        
-        for (let key in map) {
-            if (map[key] === freq) {
+    console.log(map)
+    console.log(sorted)
+
+
+    for (let key in map) {
+        for (let x of sorted) {
+            if (map[key] === x) {
                 result.push(Number(key))
-                if (result.length === k) return result
+                result = [...new Set(result)]
+                if (result.length == k) {
+                    console.log(result)
+                    return result
+                }
             }
         }
     }
 
-    return result
+    console.log(result)
+
+
+
 };
