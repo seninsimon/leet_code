0dc827e248902cfc12374ff005a49af7b95f71de
senin c simon <118550960+seninsimon@users.noreply.github.com>
2026-05-04 08:45:22 +0530

    Time: 14 ms (85.29%), Space: 73.5 MB (75.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,17 +4,29 @@
  */
 var containsDuplicate = function(nums) {
 
-    let seen = new Set()
+    // let seen = [... new Set(nums)]
+    // console.log(seen)
+    // console.log(nums)
+    // if(seen.length !== nums.length) 
+    // {
+    //     return true
+    // }
+    // else
+    // {
+    //     return false
+    // }
 
-    for(let num of nums)
-    {
-        if(seen.has(num))
-        {
-            return true
-        }
+     let seen = new Set()
+
+     for(let num of nums)
+     {
+        if(seen.has(num)) return true
         seen.add(num)
-    }
+     }
 
-    return false
+     return false
+
+
+
     
 };
